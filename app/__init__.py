3c7fbd77ccf6e5d91493014dc7ed8550df44908f
OliinykAndrii <andrii.oliinyk.23@pnu.edu.ua>
2025-10-23 23:41:37 +0300

    add products blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,9 @@ app = Flask(__name__)
 app.config.from_pyfile("../config.py")
 
 from . import views
+
+from .users import views
+app.register_blueprint(views.users_bp)
+
+from .posts import post_bp
+app.register_blueprint(post_bp)
